e9382a09eb55ac8c94873308f38e2b755e436429
natenka <nataliya.samoylenko@gmail.com>
2019-04-19 14:04:26 +0000

    TOC changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'alabaster'
 #html_theme_options = {
 #    "github_user": "natenka",
 #    "github_repo": "pyneng-book",
