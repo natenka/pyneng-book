111121a09bd942c83924ec14d8fe68eb2723cc93
natenka <nataliya.samoylenko@gmail.com>
2019-04-19 14:44:59 +0000

    TOC changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'python_docs_theme'
+html_theme = 'sphinx_rtd_theme'
 #html_sidebars = {
 #    "**": ["about.html", "searchbox.html", "localtoc.html", "navigation.html"]
 #}
