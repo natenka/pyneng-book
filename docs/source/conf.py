c7e1c6932e4fd03bedd0ec5f37bd0df79d947a28
natenka <nataliya.samoylenko@gmail.com>
2019-04-19 12:50:58 +0000

    TOC changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,6 +55,16 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'relations.html',
+        'searchbox.html',
+        'localtoc.html',
+        'globaltoc.html',
+    ]
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
