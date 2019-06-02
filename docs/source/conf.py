b28f36b632137fc80a440ebfee811af6b5a8e5d7
natenka <nataliya.samoylenko@gmail.com>
2019-06-02 15:34:33 +0000

    Update conf.py. Latex font

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,4 +81,13 @@ latex_elements = {
 \setsansfont{DejaVu Serif}
 \setmonofont{DejaVu Sans Mono}
 ''',
+    'preamble': r'''
+\usepackage[titles]{tocloft}
+\cftsetpnumwidth {1.25cm}\cftsetrmarg{1.5cm}
+\setlength{\cftchapnumwidth}{0.75cm}
+\setlength{\cftsecindent}{\cftchapnumwidth}
+\setlength{\cftsecnumwidth}{1.25cm}
+''',
+    'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
+    'printindex': r'\footnotesize\raggedright\printindex',
 }
