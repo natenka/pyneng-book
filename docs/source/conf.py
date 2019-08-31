7a4dea92c14227f25d6a4d3c699dc930da082c27
natenka <nataliya.samoylenko@gmail.com>
2019-08-31 10:42:02 +0000

    Add Last updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,6 +68,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_last_updated_fmt = ''
 
 # -- Options for LaTeX output ---------------------------------------------
 
