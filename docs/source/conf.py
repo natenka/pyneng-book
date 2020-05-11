61166c7528abffc805d3e58dfab94b220367f07b
Наташа Самойленко <nataliya.samoylenko@gmail.com>
2020-05-11 09:19:21 +0300

    add  "sphinx.ext.imgconverter" for svg/gif images pdf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ release = '3.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.imgconverter"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
